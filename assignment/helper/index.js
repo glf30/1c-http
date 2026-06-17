33664939e563d9513bd616976bb44fe0f1610e31
Greyson <greyson.frazier@gmail.com>
2026-06-17 10:17:47 -0400

    Update index.js

--- assignment/helper/index.js
+++ assignment/helper/index.js
@@ -1,1 +1,2 @@
-// Your Code Here
+// Your Code Here - use this file to create a function that will grab a random item from an array.  
+// That function should be utilized inside the file containing your server.  
